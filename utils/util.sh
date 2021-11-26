566cb3accbbb724d59c79dba8ef578af21a2d57e
nickboldt <nboldt@redhat.com>
2021-11-26 12:21:32 -0400

    fix: tweak console output from check-branches.sh and util.sh#verifyBranchExistsWithTimeout(); also actually use local variables instead of  and
    Change-Id: I3c118f185942a4eb716848bbc9c4fb915a691052
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- utils/util.sh
+++ utils/util.sh
@@ -7,16 +7,17 @@ verifyBranchExistsWithTimeout()
     count=1
     (( timeout_intervals=this_timeout*3 ))
     while [[ $count -le $timeout_intervals ]]; do # echo $count
-        echo "       [$count/$timeout_intervals] Verify branch ${2} in repo ${1} exists..." 
+        echo -n "       [$count/$timeout_intervals] Check ${this_repo%.git}/tree/${this_branch} ..." 
         # check if the branch exists
         branchExists=$(git ls-remote --heads "${this_repo}" "${this_branch}" | wc -l)
-        if [[ ${branchExists} -eq 1 ]]; then break; fi
+        if [[ ${branchExists} -eq 1 ]]; then echo " found."; break; fi
         (( count=count+1 ))
         sleep 20s
+        echo ""
     done
     # or report an error
     if [[ ${branchExists} -eq 0 ]]; then
-        echo "[ERROR] Did not find branch ${2} in repo ${1} after ${this_timeout} minutes"
+        echo "[ERROR] Branch ${this_repo%.git}/tree/${this_branch} not found after ${this_timeout} minutes"
         return 1;
     fi
 }
